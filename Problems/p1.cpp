bc93e3b82026b051d33b77884359b52f69dc0b16
Silence Turn <code.talha.dev1@gmail.com>
2025-11-30 14:39:22 +0500

    Print Stars [ asterisk ] in specific arrangement

---
+++ Problems/p1.cpp
@@ -0,0 +1,13 @@
+// Print Stars in sequence ---> 4,3,2,1.
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"****\n***\n**\n*\n";
+    // OR 
+    cout<< "****\n";
+    cout<<"***\n";
+    cout<<"**\n";
+    cout<<"*\n"; 
+    return 0;
+}
