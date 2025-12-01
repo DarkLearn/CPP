7402479ed1b1662c54631b1b33933d8179fe4b37
Silence Turn <code.talha.dev1@gmail.com>
2025-12-01 11:46:16 +0500

    Take cost of three items and print bill with GST tax and also print except GST tax

--- Problems/p5.cpp
+++ Problems/p5.cpp
@@ -12,7 +12,9 @@ int main (){
     
     float tax = (p + pe + er)*(18.00/100.00); // GST Tax
     float bill = tax + (p + pe + er);
+    float bill0 = p + pe + er;
     
-    cout << "Your bill: "<< bill << endl;
+    cout << "Your bill with GST tax: "<< bill << endl;
+    cout << "Your bill except GST tax: "<< bill0 << endl;
     return 0;
 }
