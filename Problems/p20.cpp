bd08b4c0839c641224b63bd58e91136413caabfd
Silence Turn <code.talha.dev1@gmail.com>
2025-12-25 09:55:10 +0500

    Print first N natural number using for loop

---
+++ Problems/p20.cpp
@@ -0,0 +1,14 @@
+// print first N natural number using for loop.
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cout << i << endl;
+    return 0;
+}
