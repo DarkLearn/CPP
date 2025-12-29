cef11d03f27f6cb8e5172cfb153d97b2fe1fd755
Dark Learn <code.talha.dev1@gmail.com>
2025-12-29 16:35:09 +0500

    Print digits of given number in reverse using while

---
+++ Problems/p27.cpp
@@ -0,0 +1,22 @@
+// Print digits of given number in reverse using while loop.
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n;
+     int dig;
+    cout << "Enter number: ";
+    cin >> n;
+
+    int i = 1;
+    while (n > 0) // for just positive numbers
+    {
+        dig = n % 10;
+        n /= 10;
+        cout << dig << " ";
+        i++;
+    }
+    
+    return 0;
+}
