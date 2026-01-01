1c1ad005b4069b42ce410f68ec161347acaa51b1
Dark Learn <code.talha.dev1@gmail.com>
2026-01-01 08:55:58 +0500

    Take number from user if number is multiple of 10 then End of program otherwise take again number from user again and again using do-while

---
+++ Problems/p29.cpp
@@ -0,0 +1,20 @@
+// Write a program where user can keep entering numbers till they multiple of 10.
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n;
+    do
+    {
+        cout << "Enter number: ";
+        cin >> n;
+        if (n % 10 == 0)
+        {
+            break;
+        }
+        cout << "You entered " << n << endl;
+    } while (true);
+
+    return 0;
+}
